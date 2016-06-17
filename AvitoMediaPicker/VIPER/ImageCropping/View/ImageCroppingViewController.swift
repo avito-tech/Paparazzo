f538102171cd9ce0164227c32a4e1f75232b84f9
Andrey Yutkin <ayutkin@avito.ru>
2016-06-17 11:41:07 +0300

    Remove unnecessary .h file. Implement stub ImageCroppingView

--- AvitoMediaPicker/VIPER/ImageCropping/View/ImageCroppingViewController.swift
+++ AvitoMediaPicker/VIPER/ImageCropping/View/ImageCroppingViewController.swift
@@ -1,5 +1,21 @@
 import UIKit
 
 final class ImageCroppingViewController: BaseViewControllerSwift, ImageCroppingViewInput {
+    
+    private let imageCroppingView = ImageCroppingView()
+    
     // MARK: - CroppingViewInput
+    override init() {
+        super.init()
+        title = "Crop"
+    }
+    
+    override func loadView() {
+        view = imageCroppingView
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: false)    // временно, просто чтобы не застревать на кропе
+    }
 }
