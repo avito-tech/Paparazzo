eba730353aef012a7d334799cbe2bc0d7e5fff0d
Andrey Yutkin <ayutkin@avito.ru>
2016-06-30 17:21:20 +0300

    Fix scrolling to camera on camera thumbnail tapping

--- AvitoMediaPicker/Classes/VIPER/MediaPicker/Presenter/MediaPickerPresenter.swift
+++ AvitoMediaPicker/Classes/VIPER/MediaPicker/Presenter/MediaPickerPresenter.swift
@@ -101,6 +101,7 @@ final class MediaPickerPresenter: MediaPickerModule, ImageCroppingModuleOutput {
         
         view?.onReturnToCameraTap = { [weak self] in
             self?.view?.setMode(.Camera)
+            self?.view?.scrollToCameraThumbnail(animated: true)
         }
         
         view?.onCameraToggleButtonTap = { [weak self] in
