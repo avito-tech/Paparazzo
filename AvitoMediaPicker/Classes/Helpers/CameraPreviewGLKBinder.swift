307d71d445dacd4e9714019e6e71d2091411a000
Andrey Yutkin <ayutkin@avito.ru>
2016-07-22 17:28:05 +0300

    Remove unnecessary code

--- AvitoMediaPicker/Classes/Helpers/CameraPreviewGLKBinder.swift
+++ AvitoMediaPicker/Classes/Helpers/CameraPreviewGLKBinder.swift
@@ -67,12 +67,6 @@ final class CameraOutputGLKBinder {
         
         view.enableSetNeedsDisplay = false
         
-        // because the native video image from the back camera is in UIDeviceOrientationLandscapeLeft (i.e. the home button is on the right),
-        // we need to apply a clockwise 90 degree transform so that we can draw the video preview as if we were in a landscape-oriented view;
-        // if you're using the front camera and you want to have a mirrored preview (so that the user is seeing themselves in the mirror),
-        // you need to apply an additional horizontal flip (by concatenating CGAffineTransformMakeScale(-1.0, 1.0) to the rotation transform)
-//        view.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
-        
         // bind the frame buffer to get the frame buffer width and height;
         // the bounds used by CIContext when drawing to a GLKView are in pixels (not points),
         // hence the need to read from the frame buffer's width and height;
