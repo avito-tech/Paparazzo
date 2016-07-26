343f28c9417b70132396977e84fba4b37cff045f
Andrey Yutkin <ayutkin@avito.ru>
2016-07-26 17:38:06 +0300

    Implement custom slider [WIP]

--- AvitoMediaPicker/Classes/VIPER/ImageCropping/View/RotationSliderView.swift
+++ AvitoMediaPicker/Classes/VIPER/ImageCropping/View/RotationSliderView.swift
@@ -1,18 +1,21 @@
 import UIKit
 
-final class RotationSliderView: UIView {
+final class RotationSliderView: UIView, UIScrollViewDelegate {
     
     // MARK: - Subviews
     
-    private let slider = UISlider()
+    private let scrollView = UIScrollView()
+    private let scaleView = SliderScaleView()
+    private let thumbView = UIImageView()
     
     // MARK: - Constants
     
     private let divisionsCount = 17
-    private let divisionWidth = CGFloat(2)
     
-    // Зависит от размеров непрозрачной части thumb'а слайдера
-    private let scaleInsets = UIEdgeInsets(top: 0, left: 21, bottom: 0, right: 21)
+    // MARK: - Properties
+    
+    private var minimumValue: Float = 0
+    private var maximumValue: Float = 1
     
     // MARK: - UIView
     
@@ -22,9 +25,70 @@ final class RotationSliderView: UIView {
         backgroundColor = .clearColor()
         contentMode = .Redraw
         
-        setUpSlider()
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.decelerationRate = UIScrollViewDecelerationRateFast
+        scrollView.delegate = self
         
-        addSubview(slider)
+        thumbView.image = UIImage(
+            named: "rotation-slider-thumb",
+            inBundle: NSBundle(forClass: self.dynamicType),
+            compatibleWithTraitCollection: nil
+        )
+        
+        scrollView.addSubview(scaleView)
+        
+        addSubview(scrollView)
+        addSubview(thumbView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        scaleView.frame = CGRect(x: 0, y: 0, width: 1000, height: bounds.size.height)
+        
+        scrollView.frame = bounds
+        scrollView.contentSize = scaleView.frame.size
+        
+        thumbView.sizeToFit()
+        thumbView.center = bounds.center
+    }
+    
+    // MARK: - RotationSliderView
+    
+    var onSliderValueChange: (Float -> ())?
+    
+    func setMiminumValue(value: Float) {
+        minimumValue = value
+    }
+    
+    func setMaximumValue(value: Float) {
+        maximumValue = value
+    }
+    
+    func setValue(value: Float) {
+        scrollView.contentOffset = .zero    // TODO
+    }
+    
+    // MARK: - UIScrollViewDelegate
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        onSliderValueChange?(0.5)   // TODO
+    }
+}
+
+private final class SliderScaleView: UIView {
+    
+    let divisionsSpacing = CGFloat(14)
+    let divisionWidth = CGFloat(2)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clearColor()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -34,66 +98,19 @@ final class RotationSliderView: UIView {
     override func drawRect(rect: CGRect) {
         super.drawRect(rect)
         
-        let scaleRect = bounds.shrinked(scaleInsets)
-        let divisionsSpacing = (scaleRect.size.width - CGFloat(divisionsCount) * divisionWidth) / (CGFloat(divisionsCount) - 1)
+        let divisionsCount = Int(floor(bounds.size.width / divisionsSpacing))
         
         for i in 0 ..< divisionsCount {
             
             let rect = CGRect(
-                x: scaleRect.left + CGFloat(i) * (divisionWidth + divisionsSpacing),
-                y: scaleRect.top,
+                x: bounds.left + CGFloat(i) * (divisionWidth + divisionsSpacing),
+                y: bounds.top,
                 width: divisionWidth,
-                height: scaleRect.size.height
+                height: bounds.size.height
             )
             
             UIColor.RGBS(rgb: 217).setFill()    // TODO: transparency
             UIBezierPath(roundedRect: rect, cornerRadius: divisionWidth / 2).fill()
         }
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        slider.frame = bounds
-    }
-    
-    // MARK: - RotationSliderView
-    
-    var onSliderValueChange: (Float -> ())?
-    
-    func setMiminumValue(value: Float) {
-        slider.minimumValue = value
-    }
-    
-    func setMaximumValue(value: Float) {
-        slider.maximumValue = value
-    }
-    
-    func setValue(value: Float) {
-        slider.value = value
-    }
-    
-    // MARK: - Private
-    
-    private func setUpSlider() {
-        
-        let thumbImage = UIImage(
-            named: "rotation-slider-thumb",
-            inBundle: NSBundle(forClass: self.dynamicType),
-            compatibleWithTraitCollection: nil
-        )
-        
-        slider.setMinimumTrackImage(UIImage(), forState: .Normal)
-        slider.setMaximumTrackImage(UIImage(), forState: .Normal)
-        slider.setThumbImage(thumbImage, forState: .Normal)
-        slider.addTarget(
-            self,
-            action: #selector(onRotationSliderValueChange(_:)),
-            forControlEvents: .ValueChanged
-        )
-    }
-    
-    @objc private func onRotationSliderValueChange(sender: UISlider) {
-        onSliderValueChange?(sender.value)
     }
 }
