62db774fa5859046ef5bef976732310850260970
Andrey Yutkin <ayutkin@avito.ru>
2016-08-03 17:16:20 +0300

    Revert requesting image with progressive delivery mode on crop

--- AvitoMediaPicker/Classes/VIPER/ImageCropping/View/ImageCroppingView.swift
+++ AvitoMediaPicker/Classes/VIPER/ImageCropping/View/ImageCroppingView.swift
@@ -110,7 +110,7 @@ final class ImageCroppingView: UIView, UIScrollViewDelegate {
     }
     
     func setImage(image: ImageSource, completion: (() -> ())?) {
-        image.fullResolutionImage(deliveryMode: .Progressive) { [weak self] (image: UIImage?) in
+        image.fullResolutionImage { [weak self] (image: UIImage?) in
             if let image = image {
                 self?.previewView.setImage(image)
             }
