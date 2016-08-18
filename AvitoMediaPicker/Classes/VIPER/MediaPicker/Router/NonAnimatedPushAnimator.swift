4d3b5f4a34339250ecf04476d1c7d176c9b1b519
Ivan Bondar <ibondar@avito.ru>
2016-08-18 15:03:55 +0300

    AI-3120: Fix braces

--- AvitoMediaPicker/Classes/VIPER/MediaPicker/Router/NonAnimatedPushAnimator.swift
+++ AvitoMediaPicker/Classes/VIPER/MediaPicker/Router/NonAnimatedPushAnimator.swift
@@ -1,14 +1,12 @@
 import Marshroute
 
 final class NonAnimatedPushAnimator: NavigationTransitionsAnimator {
-    override func animatePerformingTransition(animationContext context: PushAnimationContext)
-    {
+    override func animatePerformingTransition(animationContext context: PushAnimationContext) {
         shouldAnimate = false
         super.animatePerformingTransition(animationContext: context)
     }
     
-    override func animateUndoingTransition(animationContext context: PopAnimationContext)
-    {
+    override func animateUndoingTransition(animationContext context: PopAnimationContext) {
         shouldAnimate = false
         super.animateUndoingTransition(animationContext: context)
     }
