a42e715b0c3658a98a260d85109e4485e861c22e
Andrey Yutkin <ayutkin@avito.ru>
2016-08-23 10:25:34 +0300

    Fix implementation of RemoveImageRequestOperation to support cancellation properly

--- AvitoMediaPicker/Classes/ImageSource/UrlImageSource/RemoteImageRequestOperation.swift
+++ AvitoMediaPicker/Classes/ImageSource/UrlImageSource/RemoteImageRequestOperation.swift
@@ -1,6 +1,6 @@
 import SDWebImage
 
-final class RemoteImageRequestOperation<T: InitializableWithCGImage>: AsynchronousOperation, ImageRequestIdentifiable {
+final class RemoteImageRequestOperation<T: InitializableWithCGImage>: NSOperation, ImageRequestIdentifiable {
     
     let id: ImageRequestID
     
@@ -21,49 +21,87 @@ final class RemoteImageRequestOperation<T: InitializableWithCGImage>: Asynchrono
     
     // MARK: - NSOperation
     
-    override func main() {
-        
-        imageLoadingOperation = imageManager.downloadImageWithURL(
-            url,
-            options: SDWebImageOptions(),
-            progress: { [weak self, callbackQueue, onDownloadStart = options.onDownloadStart] receivedSize, expectedSize in
-                if let onDownloadStart = onDownloadStart where self?.downloadStarted == false {
-                    dispatch_async(callbackQueue, onDownloadStart)
-                    self?.downloadStarted = true
-                }
-            },
-            completed: { [weak self, callbackQueue, resultHandler, onDownloadFinish = options.onDownloadFinish] image, error, cacheType, finished, url in
-                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
-                    
-                    let cgImage = (image as UIImage?).flatMap { self?.finalCGImage(from: $0) }
-                    
-                    dispatch_async(callbackQueue) {
-                        onDownloadFinish?()
-                        resultHandler(cgImage.flatMap { T(CGImage: $0) })
-                    }
-                    
-                    self?.state = .Finished
-                }
+    override var asynchronous: Bool {
+        return true
+    }
+    
+    override var executing: Bool {
+        get { return _executing }
+        set {
+            willChangeValueForKey("isExecuting")
+            _executing = newValue
+            didChangeValueForKey("isExecuting")
+        }
+    }
+    
+    override var finished: Bool {
+        get { return _finished }
+        set {
+            willChangeValueForKey("isFinished")
+            _finished = newValue
+            didChangeValueForKey("isFinished")
+        }
+    }
+    
+    override func start() {
+        dispatch_async(syncQueue) { 
+            
+            if self.cancelled {
+                self.finished = true
+                return
             }
-        )
+            
+            self.executing = true
+            
+            self.imageLoadingOperation = self.imageManager.downloadImageWithURL(
+                self.url,
+                options: SDWebImageOptions(),
+                progress: { receivedSize, expectedSize in
+//                debugPrint("\(url.lastPathComponent) downloaded \(Int(Float(receivedSize) / Float(expectedSize) * 100))%")
+                    if let onDownloadStart = self.options.onDownloadStart where !self.downloadStarted {
+                        dispatch_async(self.callbackQueue, onDownloadStart)
+                        self.downloadStarted = true
+                    }
+                },
+                completed: { image, error, cacheType, finished, url in
+                    debugPrint("imageLoadingOperation completed")
+                    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
+                        
+                        let cgImage = (image as UIImage?).flatMap { self.finalCGImage(from: $0) }
+                        debugPrint("requested size = \(self.options.size), imageSize = (\(CGImageGetWidth(cgImage)), \(CGImageGetHeight(cgImage)))")
+                        
+                        dispatch_async(self.callbackQueue) {
+                            self.options.onDownloadFinish?()
+                            self.resultHandler(cgImage.flatMap { T(CGImage: $0) })
+                        }
+                        
+                        dispatch_async(self.syncQueue) {
+                            self.finish()
+                        }
+                    }
+                }
+            )
+        }
     }
     
     override func cancel() {
-        super.cancel()
-        
-        imageLoadingOperation?.cancel()     // SDWebImageDownloaderOperation will not call its completion block after cancellation
-        
-        switch state {
-        
-        case .Executing:
-            if let onDownloadFinish = options.onDownloadFinish where downloadStarted {
-                dispatch_async(callbackQueue, onDownloadFinish)
-            }
+        dispatch_async(syncQueue) {
+            guard !self.finished else { return }
             
-            state = .Finished
-        
-        case .Ready, .Finished:
-            break
+            super.cancel()
+            
+            if self.executing {
+                
+                self.imageLoadingOperation?.cancel()
+                
+                // SDWebImageDownloaderOperation will not call its completion block after cancellation,
+                // so we need to call onDownloadFinish here
+                if let onDownloadFinish = self.options.onDownloadFinish where self.downloadStarted {
+                    dispatch_async(self.callbackQueue, onDownloadFinish)
+                }
+                
+                self.finish()
+            }
         }
     }
     
@@ -75,8 +113,12 @@ final class RemoteImageRequestOperation<T: InitializableWithCGImage>: Asynchrono
     private let callbackQueue: dispatch_queue_t
     
     private let imageManager = SDWebImageManager.sharedManager()
-    private var imageLoadingOperation: SDWebImageOperation?
+    private weak var imageLoadingOperation: SDWebImageOperation?
     private var downloadStarted = false
+    
+    private let syncQueue = dispatch_queue_create("ru.avito.RemoteImageRequestOperation.syncQueue", DISPATCH_QUEUE_SERIAL)
+    private var _executing = false
+    private var _finished = false
     
     private func finalCGImage(from image: UIImage) -> CGImage? {
         switch options.size {
@@ -87,5 +129,10 @@ final class RemoteImageRequestOperation<T: InitializableWithCGImage>: Asynchrono
         case .FitSize(let size):
             return image.resized(toFit: size)?.CGImage
         }
+    }
+    
+    private func finish() {
+        executing = false
+        finished = true
     }
 }
