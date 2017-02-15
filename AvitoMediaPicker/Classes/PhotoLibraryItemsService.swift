83c215678c6ac4c3dfdde0da2ce4f46f1c1dd7d7
Andrey Yutkin <ayutkin@avito.ru>
2017-02-15 15:45:57 +0300

    AI-3000: abstract PhotoLibraryInteractorImpl away from Photos.framework

--- AvitoMediaPicker/Classes/PhotoLibraryItemsService.swift
+++ AvitoMediaPicker/Classes/PhotoLibraryItemsService.swift
@@ -1,14 +1,29 @@
 import Photos
+import ImageSource
 
 protocol PhotoLibraryItemsService {
     func observeAuthorizationStatus(handler: @escaping (PHAuthorizationStatus) -> ())
-    func observePhotos(handler: @escaping (_ assets: [PHAsset], _ changes: PHFetchResultChangeDetails<PHAsset>?) -> ())
+    func observeItems(handler: @escaping (_ changes: PhotoLibraryChanges) -> ())
 }
 
 final class PhotoLibraryItemsServiceImpl: NSObject, PhotoLibraryItemsService, PHPhotoLibraryChangeObserver {
 
     private let photoLibrary = PHPhotoLibrary.shared()
     private var fetchResult: PHFetchResult<PHAsset>?
+    
+    private var _imageManager: PHImageManager?
+    
+    // Нельзя сразу создавать PHImageManager,
+    // иначе он крэшнется при деаллокации, если доступ к photo library запрещен
+    private var imageManager: PHImageManager {
+        if let imageManager = _imageManager {
+            return imageManager
+        } else {
+            let imageManager = PHImageManager()
+            _imageManager = imageManager
+            return imageManager
+        }
+    }
     
     // MARK: - Init
     
@@ -43,7 +58,7 @@ final class PhotoLibraryItemsServiceImpl: NSObject, PhotoLibraryItemsService, PH
         handler(PHPhotoLibrary.authorizationStatus())
     }
     
-    func observePhotos(handler: @escaping (_ assets: [PHAsset], _ changes: PHFetchResultChangeDetails<PHAsset>?) -> ()) {
+    func observeItems(handler: @escaping (_ changes: PhotoLibraryChanges) -> ()) {
         onPhotosChange = handler
         callObserverHandler(changes: nil)
     }
@@ -61,7 +76,7 @@ final class PhotoLibraryItemsServiceImpl: NSObject, PhotoLibraryItemsService, PH
 
     // MARK: - Private
     
-    private var onPhotosChange: ((_ assets: [PHAsset], _ changes: PHFetchResultChangeDetails<PHAsset>?) -> ())?
+    private var onPhotosChange: ((_ changes: PhotoLibraryChanges) -> ())?
     private var onAuthorizationStatusChange: ((PHAuthorizationStatus) -> ())?
     
     private func setUpFetchRequest() {
@@ -78,8 +93,27 @@ final class PhotoLibraryItemsServiceImpl: NSObject, PhotoLibraryItemsService, PH
         callObserverHandler(changes: nil)
     }
 
-    private func callObserverHandler(changes: PHFetchResultChangeDetails<PHAsset>?) {
-        onPhotosChange?(assetsFromFetchResult(), changes)
+    private func callObserverHandler(changes phChanges: PHFetchResultChangeDetails<PHAsset>?) {
+        
+        let changes: PhotoLibraryChanges
+        
+        if let phChanges = phChanges {
+            changes = photoLibraryChanges(from: phChanges)
+        
+        } else {
+        
+            let items = photoLibraryItems(from: assetsFromFetchResult())
+            
+            changes = PhotoLibraryChanges(
+                removedIndexes: IndexSet(),
+                insertedItems: items.enumerated().map { (index: $0, item: $1) },
+                updatedItems: [],
+                movedIndexes: [],
+                itemsAfterChanges: items
+            )
+        }
+        
+        onPhotosChange?(changes)
     }
     
     private func assetsFromFetchResult() -> [PHAsset] {
@@ -88,5 +122,67 @@ final class PhotoLibraryItemsServiceImpl: NSObject, PhotoLibraryItemsService, PH
             images.append(asset)
         })
         return images
+    }
+    
+    private func photoLibraryItems(from assets: [PHAsset]) -> [PhotoLibraryItem] {
+        return assets.map(photoLibraryItem)
+    }
+    
+    private func photoLibraryItem(from asset: PHAsset) -> PhotoLibraryItem {
+        return PhotoLibraryItem(
+            identifier: asset.localIdentifier,
+            image: PHAssetImageSource(asset: asset, imageManager: imageManager)
+        )
+    }
+    
+    private func photoLibraryChanges(from changes: PHFetchResultChangeDetails<PHAsset>)
+        -> PhotoLibraryChanges
+    {
+        var assets = [PHAsset?]()
+        
+        var insertedObjects = [(index: Int, item: PhotoLibraryItem)]()
+        var insertedObjectIndex = changes.insertedObjects.count - 1
+        
+        var updatedObjects = [(index: Int, item: PhotoLibraryItem)]()
+        var updatedObjectIndex = changes.changedObjects.count - 1
+        
+        var movedIndexes = [(from: Int, to: Int)]()
+        
+        changes.fetchResultBeforeChanges.enumerateObjects(using: { object, _, _ in
+            assets.append(object)
+        })
+        
+        changes.removedIndexes?.reversed().forEach { index in
+            assets.remove(at: index)
+        }
+        
+        changes.insertedIndexes?.reversed().forEach { index in
+            guard insertedObjectIndex >= 0 else { return }
+            let asset = changes.insertedObjects[insertedObjectIndex]
+            insertedObjects.append((index: index, item: photoLibraryItem(from: asset)))
+            insertedObjectIndex -= 1
+        }
+        
+        changes.changedIndexes?.reversed().forEach { index in
+            guard updatedObjectIndex >= 0 else { return }
+            let asset = changes.changedObjects[updatedObjectIndex]
+            updatedObjects.append((index: index, item: self.photoLibraryItem(from: asset)))
+            updatedObjectIndex -= 1
+        }
+        
+        changes.enumerateMoves { from, to in
+            movedIndexes.append((from: from, to: to))
+        }
+        
+        let nonNilAssets = assets.flatMap {$0}
+        assert(nonNilAssets.count == assets.count, "Objects other than PHAsset are not supported")
+        
+        return PhotoLibraryChanges(
+            removedIndexes: changes.removedIndexes ?? IndexSet(),
+            insertedItems: insertedObjects,
+            updatedItems: updatedObjects,
+            movedIndexes: movedIndexes,
+            itemsAfterChanges: photoLibraryItems(from: nonNilAssets)
+        )
     }
 }
