a3cf19f796cb53484cc71887536098bd875f9757
Vadim Smal <vvsmal@avito.ru>
2017-03-22 14:43:38 +0300

    AI-5044: Refact ExamplePresenter

--- Example/PaparazzoExample/Example/Presenter/ExamplePresenter.swift
+++ Example/PaparazzoExample/Example/Presenter/ExamplePresenter.swift
@@ -57,40 +57,49 @@ final class ExamplePresenter {
 
         let cropCanvasSize = CGSize(width: 1280, height: 960)
         
-        self.router.showMediaPicker(items: items, selectedItem: items.last, maxItemsCount: 20, cropCanvasSize: cropCanvasSize) { module in
+        self.router.showMediaPicker(
+            items: items,
+            selectedItem: items.last,
+            maxItemsCount: 20,
+            cropCanvasSize: cropCanvasSize,
+            configuration: { [weak self] module in
+                self?.configureMediaPicker(module: module)
+            }
+        )
+    }
+    
+    func configureMediaPicker(module: MediaPickerModule) {
+        module.onItemsAdd = { _ in debugPrint("mediaPickerDidAddItems") }
+        module.onItemUpdate = { _ in debugPrint("mediaPickerDidUpdateItem") }
+        module.onItemRemove = { _ in debugPrint("mediaPickerDidRemoveItem") }
+        
+        module.setContinueButtonTitle("Готово")
+        
+        module.onCancel = { [weak module] in
+            module?.dismissModule()
+        }
+        
+        module.onFinish = { [weak module] items in
+            debugPrint("media picker did finish with \(items.count) items:")
+            items.forEach { debugPrint($0) }
+            self.items = items
+            module?.dismissModule()
             
-            module.onItemsAdd = { _ in debugPrint("mediaPickerDidAddItems") }
-            module.onItemUpdate = { _ in debugPrint("mediaPickerDidUpdateItem") }
-            module.onItemRemove = { _ in debugPrint("mediaPickerDidRemoveItem") }
-            
-            module.setContinueButtonTitle("Готово")
-            
-            module.onCancel = { [weak module] in
-                module?.dismissModule()
+            items.first?.image.fullResolutionImageData { data in
+                debugPrint("first item data size = \(data?.count ?? 0)")
+                let url = URL(fileURLWithPath: NSTemporaryDirectory() + "/crop_test1.jpg")
+                try! data?.write(to: url, options: [.atomic])
             }
             
-            module.onFinish = { [weak module] items in
-                debugPrint("media picker did finish with \(items.count) items:")
-                items.forEach { debugPrint($0) }
-                self.items = items
-                module?.dismissModule()
-                
-                items.first?.image.fullResolutionImageData { data in
-                    debugPrint("first item data size = \(data?.count ?? 0)")
-                    let url = URL(fileURLWithPath: NSTemporaryDirectory() + "/crop_test1.jpg")
-                    try! data?.write(to: url, options: [.atomic])
-                }
-                
-                let options = ImageRequestOptions(
-                    size: .fitSize(CGSize(width: 1000, height: 1000)),
-                    deliveryMode: .best
-                )
-                
-                items.first?.image.requestImage(options: options) { (result: ImageRequestResult<UIImage>) in
-                    let data = result.image.flatMap { UIImagePNGRepresentation($0) }
-                    let url = URL(fileURLWithPath: NSTemporaryDirectory() + "/crop_test2.jpg")
-                    try! data?.write(to: url, options: [.atomic])
-                }
+            let options = ImageRequestOptions(
+                size: .fitSize(CGSize(width: 1000, height: 1000)),
+                deliveryMode: .best
+            )
+            
+            items.first?.image.requestImage(options: options) { (result: ImageRequestResult<UIImage>) in
+                let data = result.image.flatMap { UIImagePNGRepresentation($0) }
+                let url = URL(fileURLWithPath: NSTemporaryDirectory() + "/crop_test2.jpg")
+                try! data?.write(to: url, options: [.atomic])
             }
         }
     }
