0692c0339d871c87a2c43eb7c9e67658205cc473
Timofey Khomutnikov <tnkhomutnikov@avito.ru>
2017-08-25 17:55:37 +0300

    SEL-388: Improve debouncer codestyle

--- Paparazzo/Core/Utilities/Debouncer.swift
+++ Paparazzo/Core/Utilities/Debouncer.swift
@@ -1,18 +1,3 @@
-// 1--------2----3--4-5-6---------789-------
-// ^delay.  ^dela^de^d^d^delay.   ^^^delay.
-// ------1--------------------6-----------9-
-//
-// http://rxmarbles.com/#debounce
-//
-// Real life example:
-//
-// Л-е-с-на--я------------------------------
-// ^d^d^d^^de^delay.
-// ----------------suggest("Лесная")--------
-//
-// NOTE: Access to the resource should be syncronized! See cancel() below.
-//
-
 public protocol Debouncable {
     func debounce(_ closure: @escaping () -> ())
     func cancel()
@@ -41,34 +26,7 @@ public final class Debouncer: Debouncable {
         }
     }
     
-    // Text input:        --Л-е-с--ная------- ("Лесная")
-    // As events:         --x-x-x--xxx-------
-    // Debounce time:       ^-^-^--^^^----.
-    // Debounced:         ----------------x--
-    // User taps cancel:  -------------y-----
-    //
-    // Expected result:   -------------y-----
-    // Actual result:     -------------y--x-- (without calling cancel())
-    //
-    // Convenience function: cancel()
-    //
-    // Note: in ideal world with RX you don't have to use this.
-    // It should be a single stream:
-    //
-    // Text input:        --Л-е-с--ная------- ("Лесная")
-    // As events:         --x-x-x--xxx-------
-    // User taps cancel:  -------------y-----
-    //
-    // A single stream:
-    // All this merged:   --z-z-z--zzz-z-----
-    // Debounce time:       ^-^-^--^^^-^----.
-    // Debounced:         ------------------z ("")
-    //
-    // If data streams aint a mess you will be happy without cancel().
-    // However, now in our app data streams are a mess.
-    
     public func cancel() {
-        // Preempt current operation in debouncer to get expected result as in the example above.
         debounce {}
     }
 }
