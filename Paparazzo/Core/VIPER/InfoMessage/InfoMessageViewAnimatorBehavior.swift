d64400c84d6532b30147a02306caf3a1d3eee749
Timofey Khomutnikov <tnkhomutnikov@avito.ru>
2017-09-18 13:55:08 +0300

    AI-7183: Fix message on ipad

--- Paparazzo/Core/VIPER/InfoMessage/InfoMessageViewAnimatorBehavior.swift
+++ Paparazzo/Core/VIPER/InfoMessage/InfoMessageViewAnimatorBehavior.swift
@@ -11,7 +11,13 @@ final class DefaultInfoMessageAnimatorBehavior: InfoMessageAnimatorBehavior {
     func configure(messageView: UIView, in container: UIView) {
         messageView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
         messageView.alpha = 0
-        messageView.bottom = container.height - 20
+        
+        // Hardcoded, because the camera camera controls is located above all views on iPad
+        var bottomInset: CGFloat = 20
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            bottomInset = 154
+        }
+        messageView.bottom = container.height - bottomInset
         messageView.centerX = ceil(container.width / 2)
     }
     
