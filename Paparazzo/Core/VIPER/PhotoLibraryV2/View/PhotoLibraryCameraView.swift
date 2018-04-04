d23f4a48a35a05bfabfa877af1a40c5ae90a8559
Timofey Khomutnikov <tnkhomutnikov@avito.ru>
2018-04-04 14:08:38 +0300

    SEL-1787: Add camera header view

---
+++ Paparazzo/Core/VIPER/PhotoLibraryV2/View/PhotoLibraryCameraView.swift
@@ -0,0 +1,77 @@
+import UIKit
+import ImageSource
+
+class PhotoLibraryCameraView: UIView {
+    
+    // MARK: - Subviews
+    private var cameraOutputView: CameraOutputView?
+    private let button = UIButton()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .black
+        layer.cornerRadius = 6
+        layer.masksToBounds = true
+        
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(onButtonTap(_:)), for: .touchUpInside)
+        
+        addSubview(button)
+        
+        setAccessibilityId(.cameraThumbnailCell)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - PhotoLibraryCameraView
+    var onTap: (() -> ())?
+    
+    func setCameraIcon(_ icon: UIImage?) {
+        button.setImage(icon, for: .normal)
+    }
+    
+    func setOutputParameters(_ parameters: CameraOutputParameters) {
+        
+        let newCameraOutputView = CameraOutputView(
+            captureSession: parameters.captureSession,
+            outputOrientation: parameters.orientation
+        )
+        
+        newCameraOutputView.layer.cornerRadius = 6
+        cameraOutputView?.removeFromSuperview()
+        
+        insertSubview(newCameraOutputView, belowSubview: button)
+        
+        self.cameraOutputView = newCameraOutputView
+        
+        // AI-3610: костыль для iPhone 4, чтобы не было белой рамки вокруг ячейки.
+        // Если ставить clearColor, скругление углов теряется на iOS 9.
+        self.backgroundColor = UIColor.white.withAlphaComponent(0.1)
+    }
+    
+    func setOutputOrientation(_ orientation: ExifOrientation) {
+        cameraOutputView?.orientation = orientation
+    }
+    
+    // MARK: - Layout
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let insets = UIEdgeInsets(top: 0.5, left: 0.5, bottom: 0.5, right: 0.5)
+        
+        cameraOutputView?.frame = UIEdgeInsetsInsetRect(bounds, insets)
+        button.frame = bounds
+    }
+    
+    // MARK: - Private
+    
+    @objc private func onButtonTap(_ sender: UIButton) {
+        onTap?()
+    }
+}
