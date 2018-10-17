f85e55157d20256e9387e6162e8c9e7ad4f48a17
Andrey Yutkin <ayutkin@avito.ru>
2018-10-17 15:01:26 +0300

    Move ImageSource away from this repo

--- ImageSource/Core/CIContext+XCode8Workaround.m
+++
@@ -1,9 +0,0 @@
-#import "CIContext+XCode8Workaround.h"
-
-@implementation CIContext (XCode8Workaround)
-
-+ (CIContext *)fixed_contextWithOptions:(NSDictionary<NSString *, id> *)options {
-    return [CIContext contextWithOptions:options];
-}
-
-@end
--- ImageSource/Core/InitializableWithCGImage.swift
+++
@@ -1,6 +0,0 @@
-public protocol InitializableWithCGImage {
-    // Если хотим совсем ни к чему не привязываться (отвязаться от Core Graphics),
-    // нужно создать свою структуру, представляющую bitmap, а потом реализовать
-    // для UIImage и NSImage конструкторы, позволяющие инициализировать их из этой структуры.
-    init(cgImage: CGImage)
-}
