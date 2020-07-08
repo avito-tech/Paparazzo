572225494e89b531cec9ef3ac4e60ea04c1ef6e6
Andrey Kozlov <aankozlov@avito.ru>
2020-07-08 10:40:35 +0300

    MDP-1337: Extend AccessDeniedView visual configuration

--- Paparazzo/Core/VIPER/PhotoLibrary/View/AccessDeniedUITheme.swift
+++ Paparazzo/Core/VIPER/PhotoLibrary/View/AccessDeniedUITheme.swift
@@ -1,5 +1,8 @@
 public protocol AccessDeniedViewTheme {
     var accessDeniedBackgroundColor: UIColor { get }
+    var accessDeniedTitleColor: UIColor { get }
+    var accessDeniedMessageColor: UIColor { get }
+    var accessDeniedButtonTextColor: UIColor { get }
     var accessDeniedTitleFont: UIFont { get }
     var accessDeniedMessageFont: UIFont { get }
     var accessDeniedButtonFont: UIFont { get }
