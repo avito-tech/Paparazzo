4eb148b717c60da1f52accb295635826dd2f1461
Yaroslav Zavyalov <yozavyalov@avito.ru>
2025-05-14 19:37:57 +0300

    SEL-34520: Удалил лишнее расширение

--- Sources/Paparazzo/PhotoLibraryV2/Assembly/PhotoLibraryV2MarshrouteAssembly.swift
+++ Sources/Paparazzo/PhotoLibraryV2/Assembly/PhotoLibraryV2MarshrouteAssembly.swift
@@ -16,33 +16,6 @@ public protocol PhotoLibraryV2MarshrouteAssembly: AnyObject {
     ) -> UIViewController
 }
 
-public extension PhotoLibraryV2MarshrouteAssembly {
-    func module(
-        mediaPickerData: MediaPickerData,
-        selectedItems: [PhotoLibraryItem],
-        routerSeed: RouterSeed,
-        isNewFlowPrototype: Bool,
-        configure: (PhotoLibraryV2Module) -> (),
-        onCameraV3InitializationMeasurementStart: (() -> ())?,
-        onCameraV3InitializationMeasurementStop: (() -> ())?,
-        onCameraV3DrawingMeasurementStart: (() -> ())?,
-        onCameraV3DrawingMeasurementStop: (() -> ())?
-    ) -> UIViewController {
-        module(
-            mediaPickerData: mediaPickerData,
-            selectedItems: selectedItems,
-            routerSeed: routerSeed,
-            isNewFlowPrototype: isNewFlowPrototype,
-            cameraType: .cameraV3,
-            configure: configure,
-            onCameraV3InitializationMeasurementStart: onCameraV3InitializationMeasurementStart,
-            onCameraV3InitializationMeasurementStop: onCameraV3InitializationMeasurementStop,
-            onCameraV3DrawingMeasurementStart: onCameraV3DrawingMeasurementStart,
-            onCameraV3DrawingMeasurementStop: onCameraV3DrawingMeasurementStop
-        )
-    }
-}
-
 public protocol PhotoLibraryV2MarshrouteAssemblyFactory: AnyObject {
     func photoLibraryV2Assembly() -> PhotoLibraryV2MarshrouteAssembly
 }
